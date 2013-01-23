c0e4f85570565cd4eff2ff966a326f0681b5b8ff
vaclavvondruska <vaclav.vondruska@sportsdata.cz>
2013-01-23 15:35:45 +0100

    Fixed after merge

--- src/main/scala/server/ScalaDns.scala
+++ src/main/scala/server/ScalaDns.scala
@@ -41,6 +41,12 @@ object ScalaDns {
 	val records = DnsLookupService.hostToRecords(List("blah", "blah"), RecordType.NS.id, 1)
 	logger.debug(records.toString)*/
     
+    if(args.exists(_.startsWith("-user="))) {
+      val userParts = args.find(_.startsWith("-user=")).get.substring(6).split(""",""")
+      if(userParts.length != 2) println("Invalid arguments. Usage: -user=<username>,<password>")
+      else println(UserCreator(userParts(0), userParts(1)))
+    }
+	
     if(args.isEmpty || args.contains("-start")) {
 	  Bootstrap.start
     }
