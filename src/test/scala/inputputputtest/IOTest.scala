1920407b62d0aa10a65e557be53150b78ffcbe0b
vaclavvondruska <vaclav.vondruska@sportsdata.cz>
2013-01-25 09:31:18 +0100

    Modified IO test after changing Json structure (#10)

--- src/test/scala/inputputputtest/IOTest.scala
+++ src/test/scala/inputputputtest/IOTest.scala
@@ -18,22 +18,25 @@ import models.AddressHost
 import models.ExtendedDomain
 import models.WeightedIP
 import models.WeightedNS
+import java.net.URLDecoder
 
 class IOTest extends FunSpec with BeforeAndAfter with ShouldMatchers {
   
-  val path = this.getClass.getProtectionDomain.getCodeSource.getLocation.getPath + "data/"
+  val applicationRoot = "/" + new File("").getAbsolutePath.replace("""\""", "/")
+  val path = 
+    "/" + URLDecoder.decode(this.getClass.getProtectionDomain.getCodeSource.getLocation.getPath + "data/", "UTF-8").substring(applicationRoot.length + 1)
   val domainJsons = Map(
-    "example.com." -> "{\"origin\":\"example.com.\",\"ttl\":86400,\"soa\":[{\"class\":\"in\",\"at\":\"1D\",\"hosts\":[\"ns1.example.com.\",\"hostmaster.example.com.\"],\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"ns\":[{\"class\":\"in\",\"value\":\"ns1.example.com.\"},{\"class\":\"in\",\"value\":\"ns2.smokeyjoe.com.\"}],\"mx\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"a\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[\"192.168.0.1\"]},{\"class\":\"in\",\"name\":\"www\",\"value\":[\"192.168.0.2\"]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[\"192.168.0.3\"]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[\"192.168.0.4\"]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example.com.\"}]}",
-    "example.net." -> "{\"origin\":\"example.net.\",\"ttl\":86400,\"soa\":[{\"class\":\"in\",\"at\":\"1D\",\"hosts\":[\"ns1.example.net.\",\"hostmaster.example.net.\"],\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"ns\":[{\"class\":\"in\",\"value\":\"ns1.example.net.\"},{\"class\":\"in\",\"value\":\"ns2.smokeyjoe.com.\"}],\"mx\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"a\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[\"192.168.0.1\"]},{\"class\":\"in\",\"name\":\"www\",\"value\":[\"192.168.0.2\"]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[\"192.168.0.3\"]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[\"192.168.0.4\"]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example.net.\"}]}",
-    "example2.com." -> "{\"origin\":\"example2.com.\",\"ttl\":86400,\"soa\":[{\"class\":\"in\",\"at\":\"1D\",\"hosts\":[\"ns1.example2.com.\",\"hostmaster.example2.com.\"],\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"ns\":[{\"class\":\"in\",\"value\":\"ns1.example2.com.\"},{\"class\":\"in\",\"value\":\"ns2.smokeyjoe.com.\"}],\"mx\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"a\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[\"192.168.0.1\"]},{\"class\":\"in\",\"name\":\"www\",\"value\":[\"192.168.0.2\"]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[\"192.168.0.3\"]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[\"192.168.0.4\"]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example2.com.\"}]}",
-    "example2.net" -> "{\"origin\":\"example2.net.\",\"ttl\":86400,\"soa\":[{\"class\":\"in\",\"at\":\"1D\",\"hosts\":[\"ns1.example2.net.\",\"hostmaster.example2.net.\"],\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"ns\":[{\"class\":\"in\",\"value\":\"ns1.example2.net.\"},{\"class\":\"in\",\"value\":\"ns2.smokeyjoe.com.\"}],\"mx\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"a\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[\"192.168.0.1\"]},{\"class\":\"in\",\"name\":\"www\",\"value\":[\"192.168.0.2\"]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[\"192.168.0.3\"]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[\"192.168.0.4\"]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example2.net.\"}]}"
+    "example.com." -> "{\"origin\":\"example.com.\",\"ttl\":86400,\"SOA\":[{\"class\":\"in\",\"at\":\"1D\",\"mname\":\"ns1.example.com.\",\"rname\":\"hostmaster.example.com.\",\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"NS\":[{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns1.example.com.\"}]},{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns2.smokeyjoe.com.\"}]}],\"MX\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"A\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.1\"}]},{\"class\":\"in\",\"name\":\"www\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.2\"}]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.3\"}]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.4\"}]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example.com.\"}]}",
+    "example.net." -> "{\"origin\":\"example.net.\",\"ttl\":86400,\"SOA\":[{\"class\":\"in\",\"at\":\"1D\",\"mname\":\"ns1.example.net.\",\"rname\":\"hostmaster.example.net.\",\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"NS\":[{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns1.example.net.\"}]},{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns2.smokeyjoe.com.\"}]}],\"MX\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"A\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.1\"}]},{\"class\":\"in\",\"name\":\"www\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.2\"}]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.3\"}]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.4\"}]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example.net.\"}]}",
+    "example2.com." -> "{\"origin\":\"example2.com.\",\"ttl\":86400,\"SOA\":[{\"class\":\"in\",\"at\":\"1D\",\"mname\":\"ns1.example2.com.\",\"rname\":\"hostmaster.example2.com.\",\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"NS\":[{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns1.example2.com.\"}]},{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns2.smokeyjoe.com.\"}]}],\"MX\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"A\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.1\"}]},{\"class\":\"in\",\"name\":\"www\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.2\"}]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.3\"}]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.4\"}]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example2.com.\"}]}",
+    "example2.net." -> "{\"origin\":\"example2.net.\",\"ttl\":86400,\"SOA\":[{\"class\":\"in\",\"at\":\"1D\",\"mname\":\"ns1.example2.net.\",\"rname\":\"hostmaster.example2.net.\",\"serial\":\"2002022401\",\"refresh\":\"3H\",\"retry\":\"15\",\"expire\":\"1w\",\"minimum\":\"3h\"}],\"NS\":[{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns1.example2.net.\"}]},{\"class\":\"in\",\"value\":[{\"weight\":1,\"ns\":\"ns2.smokeyjoe.com.\"}]}],\"MX\":[{\"class\":\"in\",\"priority\":10,\"value\":\"mail.another.com\"}],\"A\":[{\"class\":\"in\",\"name\":\"ns1\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.1\"}]},{\"class\":\"in\",\"name\":\"www\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.2\"}]},{\"class\":\"in\",\"name\":\"bill\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.3\"}]},{\"class\":\"in\",\"name\":\"fred\",\"value\":[{\"weight\":1,\"ip\":\"192.168.0.4\"}]}],\"cname\":[{\"class\":\"in\",\"name\":\"ftp\",\"value\":\"www.example2.net.\"}]}"
   )
   
-  val pathFile = new File(path)
+  val pathFile = new File(applicationRoot + path)
   pathFile.mkdirs
   
   domainJsons.foreach {case(name, json) =>
-    val fos = new BufferedOutputStream(new FileOutputStream(new File(path + name + "json")))
+    val fos = new BufferedOutputStream(new FileOutputStream(new File(applicationRoot + path + name + "json")))
     fos.write(json.getBytes)
     fos.close
   }
@@ -88,7 +91,9 @@ class IOTest extends FunSpec with BeforeAndAfter with ShouldMatchers {
 	  
 	  pathFile.listFiles.find(file => file.getName == domain.fullName + "json") should be(None)
 	  
-	  JsonIO.storeDomain(domain, path)
+	  val clearPath = path.substring(0, path.length - 1)
+	  
+	  JsonIO.storeDomain(domain, clearPath)
 	  
 	  val domainFile = pathFile.listFiles.find(file => file.getName == domain.fullName + "json")
 	  domainFile should not be(None)
@@ -97,5 +102,5 @@ class IOTest extends FunSpec with BeforeAndAfter with ShouldMatchers {
 	}
   }
 
-  //pathFile.deleteOnExit
+  pathFile.deleteOnExit
 }
